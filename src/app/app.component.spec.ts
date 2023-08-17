f2e1364f219bb18c0eb35b8ef5d7d47be016f466
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-17 17:11:12 +0300

    feat: update app.components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have as title 'user-weather-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('user-weather-app');
+    expect(app.title).toEqual('User Weather App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('user-weather-app app is running!');
+    expect(compiled.querySelector('.title')?.textContent).toContain('User Weather App');
   });
 });
