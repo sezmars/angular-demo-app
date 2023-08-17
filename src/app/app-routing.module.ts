deecd5aaead96d73a913d3b74d9d6bdb684cd93a
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-17 17:10:46 +0300

    feat: update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {UserListComponent} from "./pages/user-list/user-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/list', pathMatch: 'full'},
+  {path: 'list', component: UserListComponent},
+  {
+    path: 'profile/:uuid', loadComponent: () => import('./pages/user-profile/user-profile.component')
+      .then(m => m.UserProfileComponent)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
