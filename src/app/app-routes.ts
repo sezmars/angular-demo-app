6fd2665a5d6f6ded39eab9a34674743e8962fb6c
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-18 21:01:01 +0300

    feat: add not found page

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,4 +1,5 @@
 import {Route} from "@angular/router";
+import {NotFoundComponent} from "~shared/not-found/not-found.component";
 
 export const APP_ROUTES: Route[] = [
   {path: '', redirectTo: 'user-list', pathMatch: 'full'},
@@ -6,5 +7,6 @@ export const APP_ROUTES: Route[] = [
     path: 'user-list',
     loadChildren: () => import('./modules/users/users.routes').then(m => m.USER_ROUTES),
   },
+  { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '404' },
 ];
