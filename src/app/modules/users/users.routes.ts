1d89789a46f4db0428054e7f8c1cb073cdb16058
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-18 21:02:00 +0300

    fix: update users path

--- src/app/modules/users/users.routes.ts
+++ src/app/modules/users/users.routes.ts
@@ -2,11 +2,9 @@ import {Route} from '@angular/router';
 import {UserListComponent} from "../../pages/user-list/user-list.component";
 
 export const USER_ROUTES: Route[] = [
-  {path: '', component: UserListComponent,
-  children: [
-    {
-      path: `profile/:uuid`, loadComponent: () => import('../../pages/user-profile/user-profile.component')
-        .then(m => m.UserProfileComponent)
-    },
-  ]},
+  {path: '', component: UserListComponent},
+  {
+    path: `profile/:uuid`, loadComponent: () => import('../../pages/user-profile/user-profile.component')
+      .then(m => m.UserProfileComponent)
+  },
 ];
