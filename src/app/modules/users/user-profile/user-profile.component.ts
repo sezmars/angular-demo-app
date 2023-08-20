575ad305a1f3538a9a87e9d39657d30825e46ed8
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-20 09:44:50 +0300

    refactor: update async logic

--- src/app/modules/users/user-profile/user-profile.component.ts
+++ src/app/modules/users/user-profile/user-profile.component.ts
@@ -1,9 +1,10 @@
-import {ChangeDetectionStrategy, Component, OnDestroy, OnInit} from '@angular/core';
-import {IUser} from "../../../interfaces/user";
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {Location} from '@angular/common';
 import {SharedComponentsModule} from "~shared/shared-components.module";
-import {LocalStorageService} from "../../../services/local-storage.service";
+import {LocalStorageService} from "~services/local-storage.service";
+import {IUser} from "~interfaces/user";
+import {Observable} from "rxjs";
 
 @Component({
   standalone: true,
@@ -15,8 +16,8 @@ import {LocalStorageService} from "../../../services/local-storage.service";
   styleUrls: ['./user-profile.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UserProfileComponent implements OnInit, OnDestroy {
-  public user!: Partial<IUser> | null;
+export class UserProfileComponent implements OnInit {
+  public user$!: Observable<IUser | null>;
   public id: string | null;
   public isLoading: boolean = false;
 
@@ -24,24 +25,20 @@ export class UserProfileComponent implements OnInit, OnDestroy {
     private router: Router,
     private location: Location,
     private activateRouter: ActivatedRoute,
-    private localStorageService: LocalStorageService,) {
+    private localStorageService: LocalStorageService) {
     this.id = this.activateRouter.snapshot.paramMap.get('uuid');
   }
 
   public async ngOnInit() {
     this.isLoading = true;
 
-    this.user = this.localStorageService.getData(this.id!)
+    this.user$ =  this.localStorageService.getData(this.id!)
     this.isLoading = false;
 
-    if (!this.user) {
+    /*if (!this.user) {
       alert('User not found')
       await this.router.navigate(['/'])
-    }
-  }
-
-  public ngOnDestroy() {
-    this.localStorageService.clearData(this.id!)
+    }*/
   }
 
   public backClicked() {
