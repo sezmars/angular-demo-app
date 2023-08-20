740e42e8b9418b9c5488bbfc0be39ff5a9541b37
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-20 08:18:18 +0300

    feat: add navigation tabs

---
+++ src/app/shared/tabs/tabs.component.ts
@@ -0,0 +1,45 @@
+import {
+  AfterContentInit,
+  ChangeDetectionStrategy,
+  Component,
+  ContentChildren,
+  EventEmitter,
+  Output,
+  QueryList
+} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import TabComponent from "~shared/tabs/tab.component";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-tabs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TabsComponent implements AfterContentInit {
+  @Output() public activeTab: EventEmitter<string> = new EventEmitter<string>
+  @ContentChildren(TabComponent) public tabs!: QueryList<TabComponent>;
+
+  constructor(private router: Router) {}
+
+  public async ngAfterContentInit() {
+    const activeTabs = this.tabs.filter(tab => tab.active);
+
+    if (activeTabs.length === 0) {
+      await this.selectTab(this.tabs.first);
+    }
+  }
+
+  public async selectTab(tab: TabComponent) {
+    if (tab) {
+      this.tabs.toArray().forEach(tab => (tab.active = false));
+      tab.active = true;
+      if (tab.tabNavigate) {
+        await this.router.navigate([tab.tabNavigate])
+      }
+    }
+  }
+}
