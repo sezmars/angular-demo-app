b5f70dd71a6c4ee9966a55cb96831c88ebc8ade3
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-20 11:57:31 +0300

    feat: add click-stop-propagation directive

---
+++ src/app/shared/directives/click-stop-propagation.directive.ts
@@ -0,0 +1,13 @@
+import {Directive, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appClickStopPropagation]',
+  standalone: true
+})
+export class ClickStopPropagationDirective {
+  @HostListener("click", ["$event"])
+  public onClick(event: MouseEvent)
+  {
+    event.stopPropagation();
+  }
+}
