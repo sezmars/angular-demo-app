327e2c4eb09e083aeeca59ab373282dd244d8c52
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-20 09:44:43 +0300

    refactor: add async logic

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -1,34 +1,58 @@
 import {Injectable} from '@angular/core';
-import {IUser} from "../interfaces/user";
+import {BehaviorSubject, Observable} from 'rxjs';
+import {IUser} from "~interfaces/user";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocalStorageService {
+  private savedUsers$: BehaviorSubject<IUser[]> = new BehaviorSubject<IUser[]>([]);
+
+  constructor() {
+    this.updateSavedUsers();
+  }
 
   public saveData(key: string, data: IUser) {
     try {
       localStorage.setItem(key, JSON.stringify(data));
+      this.updateSavedUsers();
     } catch (e) {
       console.error(e);
     }
   }
 
-  public getData(key: string): IUser | null {
+  public getData(key: string): Observable<IUser | null> {
     try {
       const data = localStorage.getItem(key);
-      return data ? JSON.parse(data) : null;
+      return data ? new BehaviorSubject<IUser>(JSON.parse(data)) : new BehaviorSubject<null>(null);
     } catch (e) {
       console.error(e);
-      return null;
+      return new BehaviorSubject<null>(null);
     }
   }
 
-  public clearData(key: string) {
+  public clearStore() {
     try {
-      localStorage.removeItem(key);
+      localStorage.clear();
+      this.updateSavedUsers();
     } catch (e) {
       console.error(e);
     }
+  }
+
+  public getSavedUsersFromLocalStorage(): Observable<IUser[]> {
+    return this.savedUsers$.asObservable();
+  }
+
+  private updateSavedUsers() {
+    const savedUsers: IUser[] = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      if (key !== null) {
+        const userData = JSON.parse(localStorage.getItem(key)!);
+        savedUsers.push(userData);
+      }
+    }
+    this.savedUsers$.next(savedUsers);
   }
 }
