f04f55f98696e70ab6544854328ec4d03281d030
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-20 08:18:30 +0300

    fix: update routes

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -2,9 +2,9 @@ import {Route} from "@angular/router";
 import {NotFoundComponent} from "~shared/not-found/not-found.component";
 
 export const APP_ROUTES: Route[] = [
-  {path: '', redirectTo: 'user-list', pathMatch: 'full'},
+  {path: '', redirectTo: 'user', pathMatch: 'full'},
   {
-    path: 'user-list',
+    path: 'user',
     loadChildren: () => import('./modules/users/users.routes').then(m => m.USER_ROUTES),
   },
   { path: '404', component: NotFoundComponent },
