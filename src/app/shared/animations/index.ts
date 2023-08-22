6b5aee9cc44c42702478085779c08667123b9f57
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-22 10:44:14 +0300

    feat: add shared animations

---
+++ src/app/shared/animations/index.ts
@@ -0,0 +1,17 @@
+import {animate, state, style, transition, trigger} from "@angular/animations";
+
+export const fadeInOut = trigger('fadeInOut', [
+    transition(':enter', [
+        style({ opacity: 0 }),
+        animate('400ms', style({ opacity: 1 })),
+    ]),
+    transition(':leave', [
+        animate('400ms', style({ opacity: 0 })),
+    ]),
+])
+
+export const cardFlip =  trigger('cardFlip', [
+    state('default', style({ transform: 'none' })),
+    state('flipped', style({ transform: 'rotateY(180deg)' })),
+    transition('default <=> flipped', animate('0.4s ease-in-out')),
+])
