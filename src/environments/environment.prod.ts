511179dee7b66ce5b6065c3e134f14953ccb58be
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-22 10:36:16 +0300

    feat: add random Quotable Api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment: IEnv = {
   production: true,
   domain: 'http://localhost:4200',
   randomUserApi: 'https://randomuser.me/api/',
+  randomQuotableApi: 'https://api.quotable.io/random',
   openMeteoApi: 'https://api.open-meteo.com/v1/forecast',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,5 +8,6 @@ export const environment: IEnv = {
   production: false,
   domain: 'http://localhost:4200',
   randomUserApi: 'https://randomuser.me/api/',
+  randomQuotableApi: 'https://api.quotable.io/random',
   openMeteoApi: 'https://api.open-meteo.com/v1/forecast',
 };
