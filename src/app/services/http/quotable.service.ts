ab008ebb8617ccc88c5e8dad52c082111ed848ac
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-22 10:38:40 +0300

    feat: add quotable service

---
+++ src/app/services/http/quotable.service.ts
@@ -0,0 +1,16 @@
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
+
+import {environment} from "~environments/environment";
+
+@Injectable()
+export class QuotableService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getRandomQuote(): Observable<Partial<{ content: string }>> {
+    return this.http.get<Partial<{ content: string }>>(`${environment.randomQuotableApi}`);
+  }
+}
