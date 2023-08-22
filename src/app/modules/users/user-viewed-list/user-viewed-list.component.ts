8f79ddb227a3ff33d405730ee19763b32e73593d
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-22 10:43:15 +0300

    feat: add tooltip

--- src/app/modules/users/user-viewed-list/user-viewed-list.component.ts
+++ src/app/modules/users/user-viewed-list/user-viewed-list.component.ts
@@ -4,18 +4,24 @@ import {Observable} from "rxjs";
 
 import {IUser} from "~interfaces/user";
 import {LocalStorageService} from "~services/data/local-storage.service";
+import {fadeInOut} from "~shared/animations";
 import {ButtonComponent} from "~shared/components/button/button.component";
 import {CardComponent} from "~shared/components/card/card.component";
+import {TooltipPosition} from "~shared/components/tooltip/tooltip.enums";
 import {WeatherComponent} from "~shared/components/weather/weather.component";
+import {TooltipDirective} from "~shared/directives/tooltip.directive";
 
 
 @Component({
   selector: 'app-user-viewed-list',
   standalone: true,
-  imports: [CommonModule, ButtonComponent, CardComponent, WeatherComponent],
+  imports: [CommonModule, ButtonComponent, CardComponent, WeatherComponent, TooltipDirective],
   templateUrl: './user-viewed-list.component.html',
   styleUrls: ['./user-viewed-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    fadeInOut
+  ],
 })
 export class UserViewedListComponent {
   public users$!: Observable<IUser[]>
@@ -26,4 +32,5 @@ export class UserViewedListComponent {
 
   public trackBy: TrackByFunction<IUser> = (_, user) => user.login.uuid
 
+  protected readonly TooltipPosition: typeof TooltipPosition = TooltipPosition;
 }
