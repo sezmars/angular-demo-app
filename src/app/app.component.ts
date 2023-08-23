f9e9d62e06f91c5e02740f6354cf7c819b14d025
rkolesnikov <roman_kolesnikov@govitall.com>
2023-08-23 09:45:11 +0300

    fix: add baseHref for github pages and home navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { APP_BASE_HREF, PlatformLocation } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { SnackbarComponent } from '~shared/components/snackbar/snackbar.component';
 import { SpinnerComponent } from '~shared/components/spinner/spinner.component';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,7 +11,16 @@ import { SpinnerComponent } from '~shared/components/spinner/spinner.component';
   standalone: true,
   imports: [SpinnerComponent, SnackbarComponent, RouterOutlet],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: APP_BASE_HREF,
+      useFactory: (s: PlatformLocation): string => s.getBaseHrefFromDOM(),
+      deps: [PlatformLocation],
+    },
+  ],
 })
 export class AppComponent {
   public title: string = 'User Weather App';
+
+  constructor(@Inject(APP_BASE_HREF) public baseHref: string) {}
 }
